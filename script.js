13991d90f08a5b91880e43f7725cba2552beea01
IFTy <137920310+iftekharjahanabadi@users.noreply.github.com>
2024-05-29 21:47:09 +0600

    my creative

---
+++ script.js
@@ -0,0 +1,8 @@
+/*================= Typing Animation ============== */
+/*var typed = new Typed(".typing",{
+    Strings:["","web Designer","web Developer","Graphic Designer","YouTuber"],
+    typespeed:100,
+    Backspeed:60,
+    loop:true
+})
+*/
